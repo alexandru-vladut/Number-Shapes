69af3316c19eb6ac83c960fdcd8591ecb6e2820c
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2023-04-10 17:26:57 +0300

    Uploaded final version.

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Number Shapes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool ePatrat(int numar)
+  {
+    num squareRoot = sqrt(numar);
+    return squareRoot.toInt() == squareRoot;
+  }
+
+  bool eCub(int numar)
+  {
+    num cubeRoot = pow(numar, 1 / 3);
+    // return cubeRoot.toInt() == cubeRoot;
+    
+    int roundedCubeRoot = cubeRoot.round();
+
+    return roundedCubeRoot * roundedCubeRoot * roundedCubeRoot == numar;
+  }
+  
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.start,
+      
+            children: <Widget>[
+      
+              const Text(
+                "Please input a number to see if it is square or triangular.",
+                style: TextStyle(
+                  fontSize: 20,
+                )
+              ),
+      
+              TextField(
+                controller: _textController,
+                keyboardType: const TextInputType.numberWithOptions(),
+              ),
+      
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          int inputNumber = int.parse(_textController.text);
+          String message = '';
+
+          if (ePatrat(inputNumber) && eCub(inputNumber)) {
+            message = "Number $inputNumber is both SQUARE and TRIANGULAR.";
+          } else if (ePatrat(inputNumber) && !eCub(inputNumber)) {
+            message = "Number $inputNumber is SQUARE.";
+          } else if (!ePatrat(inputNumber) && eCub(inputNumber)) {
+            message = "Number $inputNumber is TRIANGULAR.";
+          } else if (!ePatrat(inputNumber) && !eCub(inputNumber)) {
+            message = "Number $inputNumber is neither SQUARE or TRIANGULAR.";
+          }
+
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(inputNumber.toString()),
+                content: Text(message),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+}
